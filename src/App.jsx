5019d0695c78e025a3d08dbf19b21729d74d19a5
andriivam <andriivlasiuk2@gmail.com>
2023-02-13 11:37:19 +0100

    push side bar code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/rootLayout.jsx";
+import Login from "./Components/forms/Login.jsx";
+import Registration from "./Components/forms/Registration.jsx";
 import Tudu from "./pages/PersonalTudu.jsx";
 import HomePage from "./pages/Home";
 import Dashboard from "./pages/Dashboard.jsx";
@@ -11,6 +13,8 @@ const router = createBrowserRouter(
       {/* from origin project - example of routing*/}
       <Route index element={<HomePage/>} /> 
       <Route path="/dashboard" element={<Dashboard />} /> 
+      <Route path="/register" element={<Registration/>} /> 
+      <Route path="/Login" element={<Login/>} /> 
       <Route path="/personal/:id" element={<Tudu />} /> 
     </Route>
   )
