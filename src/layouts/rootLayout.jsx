86a79295fb18be8839e0e651e511560f8f057c0f
Munise Ben <munise0704@gmail.com>
2023-02-14 16:10:48 +0100

    fixed middle section design deleted header text (not important added hamburher icon on sidebar

--- src/layouts/rootLayout.jsx
+++ src/layouts/rootLayout.jsx
@@ -3,16 +3,13 @@ import { Outlet } from "react-router-dom";
 const RootLayout = ({ logedIn }) => {
     return (
         <div className="root-layout">
-            <header>
-                Hello Tudu's team ! Tudu or not todu, it's up to u !
-            </header>
-            <main >
-                < Outlet />
+            <header></header>
+            <main>
+                <Outlet />
             </main>
-            <footer className="">
-            </footer>
+            <footer className=""></footer>
         </div>
     );
-}
+};
 
 export default RootLayout;
