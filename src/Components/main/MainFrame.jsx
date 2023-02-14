0c22220fc2b18c1c8a6a1808f9883e94f5128757
prati <pratima.sampath@gmail.com>
2023-02-14 12:06:20 +0100

    mainframe coding

--- src/Components/main/MainFrame.jsx
+++ src/Components/main/MainFrame.jsx
@@ -1,14 +1,21 @@
 import React ,{useEffect,useState}from 'react'
 import { GetTodos } from '../../events/axiosGlobal'
+import Cookies from 'js-cookie'
 const MainFrame = () => {
-    const [tuduArray,setTuduArray] = useState([])
-      setTuduArray(GetTodos())
-    console.log(tuduArray)
+  const [tudu,setTudu] = useState([])
+  useEffect( () => {
+    async function fetchData() {
+      const data = Cookies.get("id");
+      setTudu(await GetTodos(data));
+    }
+    fetchData()
+  }, []);
   return (
     <div>
       
     </div>
   )
-}
+// category,description,duedate,entrydate,id,place,ptaskname,style,url,user_id
+
 
 export default MainFrame
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import TodoItemForm from "../Components/forms/TodoItemForm";
 import { GetTodos } from "../events/axiosGlobal";
 import Cookies from "js-cookie";
 
 const Dashboard = () => {
+  const [tudu,setTudu] = useState([])
   useEffect( () => {
     async function fetchData() {
       const data = Cookies.get("id");
-      await GetTodos(data);
+      setTudu(await GetTodos(data));
     }
     fetchData()
   }, []);
-  
+ 
   return <TodoItemForm />;
 };
 
