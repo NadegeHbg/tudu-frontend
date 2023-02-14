8d76d4be50fc49791d2eee4da9d504f724eb37b9
Arseniia <arseniyadamaksina@gmail.com>
2023-02-14 13:29:06 +0100

    Tailwind styling 2

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,17 +2,24 @@ import { useEffect } from "react";
 import TodoItemForm from "../Components/forms/TodoItemForm";
 import { GetTodos } from "../events/axiosGlobal";
 import Cookies from "js-cookie";
+import Sidebar from "../Components/main/Sidebar";
 
 const Dashboard = () => {
-  useEffect( () => {
+  useEffect(() => {
     async function fetchData() {
       const data = Cookies.get("id");
       await GetTodos(data);
     }
-    fetchData()
+    fetchData();
   }, []);
-  
-  return <TodoItemForm />;
+
+  return (
+    <div className="flex justify-between">
+      <Sidebar />
+
+      <TodoItemForm />
+    </div>
+  );
 };
 
 export default Dashboard;
