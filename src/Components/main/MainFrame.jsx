c6f3e5bd0e1e52f9787aaccfb885c3a9e2013962
prati <pratima.sampath@gmail.com>
2023-02-14 15:38:28 +0100

    list mainframe

--- src/Components/main/MainFrame.jsx
+++ src/Components/main/MainFrame.jsx
@@ -11,10 +11,31 @@ const MainFrame = () => {
     fetchData()
   }, []);
   return (
-    <div>
-      
+    <div class='header '>
+      <h3>Current Tudu</h3>
+      <div class= 'text-center'>
+        <div class= 'flex'>
+          <div class= ' border-2'>
+              <p>Tudu Name</p>
+          </div>
+          <div class=' border-2 width-1/2'>
+              <p>Description</p>
+          </div>
+          <div class='flex border-2'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+</svg>
+
+              <p>2</p>
+          </div>
+          <div class='border-2'>
+              <p>End date</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
+}
 // category,description,duedate,entrydate,id,place,ptaskname,style,url,user_id
 
 
