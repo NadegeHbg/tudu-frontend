595a1dc2c353404ac5239d216d9c6e6edd33ebe2
IGotYB <igotyb.stream@gmail.com>
2023-02-16 13:45:51 +0100

    uncoment home & set index on header

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -15,10 +15,7 @@ const Header = () => {
 
     return (
         <div className='h-20'>
-            <div className='container mx-auto flex justify-end'>
-                <button className='mt-2 p-1 border-2 border-primary' onClick={() => { navigate('/login') }}>Login</button>
-            </div>
-            <div className="wave h-26">
+            <div className="wave h-26 -z-50">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <linearGradient
                         id="gradient"
@@ -38,6 +35,9 @@ const Header = () => {
                     <path id="head1" d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" fill="url(#gradient)" />
                     <path id="head2" d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" transform="scale(-1, 1) translate(-700, 0)" fill="url(#gradient)" visibility='hidden' />
                 </svg>
+            </div>
+            <div className='container mx-auto flex justify-end z-50'>
+                <button className='mt-2 p-1 border-2 border-primary' onClick={() => { navigate('/login') }}>Login</button>
             </div>
         </div>
 
