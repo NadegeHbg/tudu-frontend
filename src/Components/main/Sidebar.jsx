aed46aec04bfd85703c6762a6b8b77c8c36dedff
IGotYB <igotyb.stream@gmail.com>
2023-02-16 14:47:50 +0100

    changes sidebar svg to heroicIcon

--- src/Components/main/Sidebar.jsx
+++ src/Components/main/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import TodoItemForm from "../forms/TodoItemForm";
+import { ArchiveBoxIcon, Bars3BottomLeftIcon, CalendarDaysIcon, CheckCircleIcon, ChevronRightIcon, MagnifyingGlassIcon, StarIcon, TagIcon } from '@heroicons/react/24/outline'
+
 
 const searchHandler = (event) => {
     event.preventDefault();
@@ -13,9 +15,10 @@ export default function Sidebar({ tudu }) {
     const handleOpen = () => {
         setOpen(!open);
     };
+    
     return (
-        <div>
-            <nav class="fixed top-0 z-50 w-full border-b  bg-gray-800 border-gray-700">
+        <div className="">
+            <nav class="fixed top-0 z-50 w-full border-b bg-gray-800 border-gray-700">
                 <div class="px-3 py-3 lg:px-5 lg:pl-3">
                     <div class="flex items-center justify-between">
                         <div className="flex justify-center ">
@@ -29,13 +32,7 @@ export default function Sidebar({ tudu }) {
                                 class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                             >
                                 <span class="sr-only">Open sidebar</span>
-                                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        clip-rule="evenodd"
-                                        fill-rule="evenodd"
-                                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                                    ></path>
-                                </svg>
+                                <Bars3BottomLeftIcon className="w-6 h-6 text-neutral-100" />
                             </button>
                         </div>
                         <div class="flex items-center ml-3">
@@ -50,7 +47,7 @@ export default function Sidebar({ tudu }) {
                 class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
                 aria-label="Sidebar"
             >
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-800">
+                <div className="h-full px-3 py-4 pt-24 overflow-y-auto bg-gray-800">
                     <ul className="space-y-2">
                         <li>
                             <form action="">
@@ -58,20 +55,7 @@ export default function Sidebar({ tudu }) {
                                     className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                                     htmlFor=" Search "
                                 >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="cyan"
-                                        className="w-6 h-6 mr-4"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                                        />
-                                    </svg>
+                                    <MagnifyingGlassIcon className="w-6 h-6 text-cyan-300" />
 
                                     <input className="flex items-center text-coolBlack font-normal" type="text" name="search" placeholder=" Search " />
                                 </label>
@@ -81,52 +65,33 @@ export default function Sidebar({ tudu }) {
 
                         <li>
                             <span className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="cyan" className="w-6 h-6">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
-                                    />
-                                </svg>
+                                <ArchiveBoxIcon className="w-6 h-6 text-cyan-300" />
 
                                 <span className="flex-1 ml-3 whitespace-nowrap">All</span>
                                 <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
                             </span>
                         </li>
+
                         <li>
                             <span className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="cyan" className="w-6 h-6">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                                    />
-                                </svg>
-
+                                <StarIcon className="w-6 h-6 text-cyan-300" />
                                 <span className="flex-1 ml-3 whitespace-nowrap">Active</span>
                                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                                     3
                                 </span>
                             </span>
                         </li>
+
                         <li>
                             <span className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="cyan" className="w-6 h-6">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                                    />
-                                </svg>
-
+                                < CalendarDaysIcon className="w-6 h-6 text-cyan-300" />
                                 <span className="flex-1 ml-3 whitespace-nowrap">Upcoming</span>
                             </span>
                         </li>
+
                         <li>
                             <span className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="cyan" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
+                                <CheckCircleIcon className="w-6 h-6 text-cyan-300" />
 
                                 <span className="flex-1 ml-3 whitespace-nowrap">Done</span>
                             </span>
@@ -136,14 +101,7 @@ export default function Sidebar({ tudu }) {
                                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                                 onClick={handleOpen}
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="cyan" className="w-6 h-6">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
-                                    />
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6z" />
-                                </svg>
+                                <TagIcon className="w-6 h-6 text-cyan-300" />
 
                                 <span className="flex-1 ml-3 whitespace-nowrap">Category</span>
                             </button>
@@ -151,16 +109,7 @@ export default function Sidebar({ tudu }) {
                                 ? (
                                       <ul>
                                           <li className="flex items-center  text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                              <svg
-                                                  xmlns="http://www.w3.org/2000/svg"
-                                                  fill="none"
-                                                  viewBox="0 0 24 24"
-                                                  strokeWidth={1.5}
-                                                  stroke="yellow"
-                                                  className="w-6 h-6 flex-1 "
-                                              >
-                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                              </svg>
+                                              < ChevronRightIcon className="w-6 h-6" />
                                               <button class="flex- mr-14 whitespace-nowrap p-2 text-base font-normal">Project</button>
                                           </li>
                                       </ul>
@@ -170,7 +119,7 @@ export default function Sidebar({ tudu }) {
                     </ul>
                 </div>
             </aside>
-            <div class="p-4 sm:ml-64"></div>
+            {/* <div class="p-4 sm:ml-64"></div> */}
         </div>
     );
 }
