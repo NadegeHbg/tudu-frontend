c6ee00c04a001829f49663ba4af7c0fe876c3ea5
Munise Ben <munise0704@gmail.com>
2023-02-20 14:35:44 +0100

    button double

--- src/Components/forms/TodoItemFormAdd.jsx
+++ src/Components/forms/TodoItemFormAdd.jsx
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { CheckIcon, EllipsisVerticalIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
-export default function TodoItemForm() {
+export default function TodoItemFormAdd() {
     const [showModal, setShowModal] = useState(false);
     const {
         register,
@@ -43,7 +43,7 @@ export default function TodoItemForm() {
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*Edit Section Header*/}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="text-3xl font-logoFont"> Edit </h3>
+                                    <h3 className="text-3xl font-logoFont"> Add </h3>
                                     <button
                                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                         onClick={() => setShowModal(false)}
