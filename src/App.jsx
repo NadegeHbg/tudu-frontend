0146616b92543b1a731eabe6784eb0a5598fddb9
IGotYB <igotyb.stream@gmail.com>
2023-02-21 10:35:55 +0100

    test of category filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/rootLayout.jsx";
-import Tudu from "./pages/PersonalTudu.jsx";
 import HomePage from "./pages/Home";
 import Dashboard from "./pages/Dashboard.jsx";
 import MainFrame from "./Components/main/MainFrame.jsx";
 import "./index.css";
+import Test from "./pages/TestPage.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -13,7 +13,7 @@ const router = createBrowserRouter(
             <Route index element={<HomePage />} />
             <Route path="/mainframe" element={<MainFrame />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/personal/:id" element={<Tudu />} />
+            <Route path="/test" element={<Test />} />
         </Route>
     )
 );
