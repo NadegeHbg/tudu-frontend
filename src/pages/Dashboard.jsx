677d79514342d65ca7847893825f38590abaf1a8
andriivam <andriivlasiuk2@gmail.com>
2023-02-28 20:03:27 +0100

    fix conflicts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,26 +10,35 @@ const Dashboard = () => {
 
     // get all tudu from back end store it here. even filtered tudu is stored here
     const [tudu, setTudu] = useState([]);
-    const [view, setView] = useState(true)
+    const [view, setView] = useState(localStorage.getItem('view') === 'true' || true);
+    const userId = Cookies.get('id');
+
+
     // eslint-disable-next-line
     useEffect(() => {
         async function fetchData() {
-            const data = Cookies.get("id");
-            setTudu(await GetTodos(data));
+            setTudu(await GetTodos(userId));
         }
         fetchData();
-    }, []);
+        if (localStorage.getItem('view') !== null) {
+            setView(localStorage.getItem('view') === 'true');
+        }
+    }, [userId]);
+
+    const toggleView = () => {
+        const newView = !view;
+        setView(newView);
+        localStorage.setItem('view', newView);
+    };
 
 
     return (
-        <div>
-            <HeaderDashboard tudu={tudu}/>
-            <div className="flex">
-                <Sidebar tudu={tudu} setTudu={setTudu}  view={view} setView={setView}/>
-                {view ? 
-                    (<MiddleSection tudu={tudu} setTudu={setTudu}  />) : (<MainFrame tudu={tudu} setTudu={setTudu} />)
-                }
-            </div>
+        <div className="flex">
+            <Sidebar tudu={tudu} setTudu={setTudu}  view={view} toggleView={toggleView} />
+            {view ? 
+                (<MiddleSection tudu={tudu} setTudu={setTudu}  />) : (<MainFrame tudu={tudu} setTudu={setTudu} />)
+            }
+
         </div>
     );
 };
